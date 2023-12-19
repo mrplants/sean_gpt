04d14f39790604c2518e8be612ed3f58751de316
Sean F <fitzgerald.sean.t@gmail.com>
2023-12-18 19:38:39 -0800

    Update distribution files and add blank line in main.py

--- sean_gpt/main.py
+++ sean_gpt/main.py
@@ -8,6 +8,7 @@ from .routers import chat_completions_openai
 app = FastAPI()
 app.include_router(chat_completions_openai.router)
 
+
 @app.on_event("startup")
 def startup_event():
     # Load environment variables from a .env file

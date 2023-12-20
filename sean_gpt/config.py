87b957da3d1644df040580c96e3214c1efee6149
Sean F <fitzgerald.sean.t@gmail.com>
2023-12-19 20:01:54 -0800

    Update Dockerfile, kub yamls, and environment configuration

--- sean_gpt/config.py
+++ sean_gpt/config.py
@@ -1,6 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="env/.env.secrets")
+load_dotenv(dotenv_path="env/.env.local")
+load_dotenv(dotenv_path="env/.env")
+
+# In a production environment, there will be no .env or .env.secrets files,
+# and the values will be set by the environment variables
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra='allow')
+    secret_key: str
+    openai_api_key: str
+    database_name: str
+    database_url: str
+    algorithm: str
+    openai_api_url: str
+    access_token_expire_minutes: int
 
 settings = Settings()

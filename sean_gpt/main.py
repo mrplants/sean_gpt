28884e016450aae37e37761a81298a710faaf84d
Sean F <fitzgerald.sean.t@gmail.com>
2023-12-19 21:06:04 -0800

    Switch to local docker build without having to go through github

--- sean_gpt/main.py
+++ sean_gpt/main.py
@@ -9,4 +9,10 @@ from .routers import authentication
 app = FastAPI()
 app.include_router(chat_completions_openai.router)
 app.include_router(authentication.router)
+
+@app.get("/test")
+def test():
+    return {"Hello": "World"}
+
+# Mount this last so that it doesn't override other routes
 app.mount("/", StaticFiles(directory=os.path.dirname(__file__)+'/static/html', html=True), name="static")

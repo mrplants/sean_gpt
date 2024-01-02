b6be7a2547d031c07b6584a6fab3d902e1f7a20e
Sean F <fitzgerald.sean.t@gmail.com>
2024-01-01 16:15:09 -0800

    Restructuring to support deployment workflow.

--- deployment/scripts/local_reset_postgres.sh
+++ deployment/scripts/local_reset_postgres.sh
@@ -2,4 +2,4 @@ kubectl delete statefulset postgres
 kubectl delete pvc postgres-volume-postgres-0
 # Get the directory of the script
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-kubectl apply -f "${SCRIPT_DIR}/postgres-set.yaml"
+kubectl apply -f "${SCRIPT_DIR}/../postgres-set.yaml"
